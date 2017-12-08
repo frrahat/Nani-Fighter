8db7136cb2e726caab7977cb6047f1e38742e559
frrahat <fr.rahat@gmail.com>
2017-12-09 01:41:54 +0600

    changed version to 1.0

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup
+
 setup(
-  name = 'nani_fighter',
-  packages = ['nani_fighter'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A Simple Game',
-  long_description = open("README.md").read(),
-  author = 'Fazle Rahat',
-  author_email = 'fr.rahat@gmail.com',
-  license='MIT',
-  url = 'https://github.com/frrahat/Nani-Fighter', # use the URL to the github repo
-  download_url = 'https://github.com/frrahat/Nani-Fighter/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['game', 'school', 'shooting'], # arbitrary keywords
-  classifiers = [
-    'Intended Audience :: End Users/Desktop',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-  ],
-  install_requires=[
-    'pygame',
-  ],
+    name='nani_fighter',
+    packages=['nani_fighter'],  # this must be the same as the name above
+    version='1.0',
+    description='A Simple Game',
+    long_description=open("README.md").read(),
+    author='Fazle Rahat',
+    author_email='fr.rahat@gmail.com',
+    license='MIT',
+    url='https://github.com/frrahat/Nani-Fighter',  # use the URL to the github repo
+    download_url='https://github.com/frrahat/Nani-Fighter/archive/1.0.tar.gz',  # I'll explain this in a second
+    keywords=['game', 'school', 'shooting'],  # arbitrary keywords
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'pygame',
+    ],
 )
