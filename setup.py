0397497b7aeea9fcede96c36487ed6e4ffd82821
frrahat <fr.rahat@gmail.com>
2017-12-09 04:50:26 +0600

    changed version to 1.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='nani_fighter',
     packages=['nani_fighter'],  # this must be the same as the name above
-    version='1.0',
+    version='1.1',
     description='A Simple Game',
     long_description=open("README.md").read(),
     author='Fazle Rahat',
     author_email='fr.rahat@gmail.com',
     license='MIT',
     url='https://github.com/frrahat/Nani-Fighter',  # use the URL to the github repo
-    download_url='https://github.com/frrahat/Nani-Fighter/archive/1.0.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/frrahat/Nani-Fighter/archive/1.1.tar.gz',  # I'll explain this in a second
     keywords=['game', 'school', 'shooting'],  # arbitrary keywords
     classifiers=[
         'Intended Audience :: End Users/Desktop',
